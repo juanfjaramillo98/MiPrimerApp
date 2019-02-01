636bb6c96851d83fd7661d851d963d6e0d41fd78
juanfjaramillo98 <juanjaraa5@gmail.com>
2019-01-31 21:00:54 -0500

    clase2

--- app/src/main/java/appmoviles/com/miprimerapp/MainActivity.java
+++ app/src/main/java/appmoviles/com/miprimerapp/MainActivity.java
@@ -1,13 +1,70 @@
 package appmoviles.com.miprimerapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_reg;
+    private Button btn_login;
+    private EditText et_user;
+    private EditText et_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_user = findViewById(R.id.et_user);
+        et_pass= findViewById(R.id.et_password);
+        tv_reg = findViewById(R.id.tv_reg);
+        tv_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Register.class); // representa la flecha de asociacion
+                startActivity(i);
+
+
+            }
+        });
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Profile.class);
+                i.putExtra("nombre",et_user.getText().toString());
+                i.putExtra("contra",et_pass.getText().toString());
+                startActivity(i);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder( this)
+                .setTitle("SALIR")
+                .setMessage("¿Desea salir?")
+                .setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+
+                    }
+                }).setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        MainActivity.super.onBackPressed();
+                    }
+                });
+        builder.show();
     }
 }
